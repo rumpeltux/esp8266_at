024f77417583a33157d7482a65358248a86c9ec8
Kaelan <kaelan.duck@gmail.com>
2015-03-17 17:14:59 +0800

    Disabled network updating

--- at/user/at_cmd.h
+++ at/user/at_cmd.h
@@ -23,7 +23,7 @@
 #include "at_ipCmd.h"
 #include "at_baseCmd.h"
 
-#define at_cmdNum   32
+#define at_cmdNum   30
 
 at_funcationType at_fun[at_cmdNum]={
   {NULL, 0, NULL, NULL, NULL, at_exeCmdNull},
@@ -55,9 +55,9 @@ at_funcationType at_fun[at_cmdNum]={
   {"+CIPSERVER", 10, NULL, NULL,at_setupCmdCipserver, NULL},
   {"+CIPMODE", 8, NULL, at_queryCmdCipmode, at_setupCmdCipmode, NULL},
   {"+CIPSTO", 7, NULL, at_queryCmdCipsto, at_setupCmdCipsto, NULL},
-  {"+CIUPDATE", 9, NULL, NULL, NULL, at_exeCmdCiupdate},
+//  {"+CIUPDATE", 9, NULL, NULL, NULL, at_exeCmdCiupdate},
   {"+CIPING", 7, NULL, NULL, NULL, at_exeCmdCiping},
-  {"+CIPAPPUP", 9, NULL, NULL, NULL, at_exeCmdCipappup},
+//  {"+CIPAPPUP", 9, NULL, NULL, NULL, at_exeCmdCipappup},
 #ifdef ali
   {"+MPINFO", 7, NULL, NULL, at_setupCmdMpinfo, NULL}
 #endif
